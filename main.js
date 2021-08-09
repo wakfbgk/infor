67e82961453bf4743690011e290e0c8ef2088c7b
wakfbgk <88644991+wakfbgk@users.noreply.github.com>
2021-08-09 11:08:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,155 @@
+function Tim_Cook() {
+    window.alert(
+        "Timothy Donald Cook (born November 1, 1960)[2] is an American business executive who has been the chief executive officer of Apple Inc. since 2011. Cook previously served as the company's chief operating officer under its co-founder Steve Jobs.[3] Cook joined Apple in March 1998 as a senior vice president for worldwide operations, and then served as the executive vice president for worldwide sales and operations.[4] He was made the chief executive on August 24, 2011, prior to Jobs' death in October of that year."
+    );
+}
+
+function Satya_Nadella() {
+    window.alert(
+        "Satya Narayana Nadella (born 19 August 1967) is an Indian-born American business executive. He is the executive chairman and CEO of Microsoft, succeeding Steve Ballmer in 2014 as CEO[2][3] and John W. Thompson in 2021 as chairman.[4][5] Before becoming CEO, he was the executive vice president of Microsoft's cloud and enterprise group, responsible for building and running the company's computing platforms.[6] "
+    );
+}
+
+function Michell_S_Dell() {
+    window.alert(
+        "Michael Saul Dell (born February 23, 1965) is an American billionaire businessman and philanthropist. He is the founder, chairman and CEO of Dell Technologies, one of the world's largest technology infrastructure companies.[1] He is ranked 25th richest in the world by Bloomberg Billionaires Index, with a net worth of US$51.7 billion as of April 17, 2021.[2] "
+    );
+}
+
+function Mark_Z() {
+    window.alert(
+        "Mark Elliot Zuckerberg (born May 14, 1984) is an American media magnate, internet entrepreneur, and philanthropist. He is known for co-founding Facebook, Inc. and serves as its chairman, chief executive officer, and controlling shareholder.[2][3] He also is a co-founder of the solar sail spacecraft development project Breakthrough Starshot and serves as one of its board members.[4]"
+    );
+}
+
+function Steve_Jobs() {
+    window.alert(
+        "Steven Paul Jobs (February 24, 1955 – October 5, 2011) was an American business magnate, industrial designer, investor, and media proprietor. He was the chairman, chief executive officer (CEO), and co-founder of Apple Inc.; the chairman and majority shareholder of Pixar; a member of The Walt Disney Company's board of directors following its acquisition of Pixar; and the founder, chairman, and CEO of NeXT. Jobs is widely recognized as a pioneer of the personal computer revolution of the 1970s and 1980s, along with his early business partner and fellow Apple co-founder Steve Wozniak."
+    );
+}
+
+function Bill_Gates() {
+    window.alert(
+        "William Henry Gates III (born October 28, 1955) is an American business magnate, software developer, investor, author, and philanthropist. He is a co-founder of Microsoft Corporation, along with his late childhood friend Paul Allen.[3][4] During his career at Microsoft, Gates held the positions of chairman, chief executive officer (CEO), president and chief software architect, while also being the largest individual shareholder until May 2014.[5] He is considered one of the best known entrepreneurs of the microcomputer revolution of the 1970s and 1980s."
+    );
+}
+
+function Sundar_Pichai() {
+    window.alert(
+        "Pichai Sundararajan (born June 10, 1972[4][5][1]), better known as Sundar Pichai (/ˈsʊndɑːr pɪˈtʃaɪ/), is an Indian-American business executive.[6] He is the chief executive officer (CEO) of Alphabet Inc. and its subsidiary Google.Born in Madras, India,[4] Pichai earned his degree from IIT Kharagpur in metallurgical engineering. Moving to the United States, he attained an M.S. from Stanford University in materials science and engineering and further attained an MBA from the Wharton School of the University of Pennsylvania, where he was named a Siebel Scholar and a Palmer Scholar, respectively.[7]"
+    );
+}
+
+function Larry_Page_and_Sergey_Brin() {
+    window.alert(
+        "Lawrence Edward Page[2][3][4] (born March 26, 1973) is an American business magnate, computer scientist and Internet entrepreneur. He is best known as one of the co-founders of Google along with Sergey Brin.[2][5] Lawrence Edward Page[2][3][4] (born March 26, 1973) is an American business magnate, computer scientist and Internet entrepreneur. He is best known as one of the co-founders of Google along with Sergey Brin.[2][5] Page was the chief executive officer of Google from 1997 until August 2001 (stepping down in favor of Eric Schmidt) then from April 2011 until July 2015 when he moved to become CEO of Alphabet Inc. (created to deliver major advancements as Google's parent company),[6] a post he held until December 4, 2019. He remains an Alphabet board member, employee, and controlling shareholder.[7] Sergey Mikhaylovich Brin (Russian: Серге́й Миха́йлович Брин, tr. Sergéj Mixájlovič Brin; born August 21, 1973) is an American business magnate, computer scientist and Internet entrepreneur. Together with Larry Page, he co-founded Google. Brin was the president of Google's parent company, Alphabet Inc., until stepping down from the role on December 3, 2019.[1] He and Page remain at Alphabet as co-founders, controlling shareholders, board members, and employees. As of April 2021, Brin is the 8th-richest person in the world, with an estimated net worth of $100.2 billion.[2]"
+    );
+}
+
+function Tim_Berners_Lee() {
+    window.alert(
+        "Sir Timothy John Berners-Lee OM KBE FRS FREng FRSA FBCS (born 8 June 1955),[1] also known as TimBL, is an English computer scientist best known as the inventor of the World Wide Web. He is a Professorial Fellow of Computer Science at the University of Oxford[2] and a professor at the Massachusetts Institute of Technology (MIT).[3][4] Berners-Lee proposed an information management system on 12 March 1989,[5][6] then implemented the first successful communication between a Hypertext Transfer Protocol (HTTP) client and server via the Internet in mid-November"
+    );
+}
+
+function debarghya() {
+    window.alert(
+        "He is a good friends he is patient and alwyas supporting you but he can be stubborn but thats not all he loves sports and he is a healthy kid too he does programming in HTML, CSS and JS he even teaches me those languages . He is one of my best friend"
+    );
+}
+
+function arav() {
+    window.alert(
+        "he is also a good friend is patient he gets pissed of when we outsmart him he loves maths, chess , cricket and he is a very good boy he is healthy but also fat he does not love coding at all sometimes he helps me in maths too he is also one of my best friend. "
+    );
+}
+
+function atharv() {
+    window.alert(
+        "he is also a good friend is patient he gets angry or freaksout when we outsmart him he loves maths, chess , cricket and he is a very good boy he is healthy but also he has quick ressistencewhich is really helpful does love coding sometimes he helps me in making my laptop both software and hardware look cool too he is also one of my best friend. "
+    );
+}
+
+function vaibhav() {
+    window.alert(
+        "he is a good boy he is mainly a gamer he play minecraft and roblox and a supporter too."
+    );
+}
+
+function bhuvan() {
+    window.alert(
+        "he is a good boy then he kinda overacts and he have to speak properly and also read the words properly he playes with deb (debarghya), viti (another friend), arav and me "
+    );
+}
+
+function siddarth() {
+    window.alert(
+        "siddarth is a good boy I don't know him much but I know that the he is a very annoying guys (no offence) and then he also paticipates in all the subjects. "
+    );
+}
+
+function anantha() {
+    window.alert(
+        "Anantha is one of my best friend i knew him from the past 3 years and then he is a best friends of bhuvan and others he plays wih us at sat and sun he is also a healthy boy ."
+    );
+}
+
+function karthik() {
+    window.alert(
+        "Karthik is a smart and healthy boy but he acts kiddish and not like a mature person he frequently uses the word fool and tries to outsmart someone but instead he makes them angry."
+    );
+}
+
+function ankita() {
+    window.alert(
+        "she is a good, smart and healthy girl she supports the person who is right and then she helps then people who are feeling down. She participates in all the subjects and also likes basketball.  "
+    );
+}
+
+function ananya() {
+    window.alert(
+        "ananya (frequently called as onion) she is a class topper (just like me) she gets good marks and then she playes basketball too."
+    );
+}
+
+function manasvi() {
+    window.alert(
+        "manasvi is a very good girl when she has inteernet issues her voice comes like chirpng like a bird and then everybody starts scolding her."
+    );
+}
+
+function deetya() {
+    window.alert(
+        "Deetya is a very tall girl (for some reason) i knew her from 2018 when i was in 3rd grade she is a bright student (but not brighter than me)."
+    );
+}
+
+function nitya() {
+    window.alert(
+        "Nitya is the shortest person in the class but yet the 2nd elder person in thhe class she i also a brght student."
+    );
+}
+
+function viti() {
+    window.alert(
+        "Viti is a mortal in roblox u name the game and in that game she will rock u he is also a bright student but unfortunately she is no more in my school but still we play with her during saturday and sunday . "
+    );
+}
+
+function prakruti() {
+    window.alert(
+        "She joined this school in 4th grade and she speaks very good hindi because she is from J & K she is also very addapted to the cold."
+    );
+}
+
+function divya() {
+    window.alert(
+        "she joined in UKG she is not good number and math she gets lost all the time during this COVID-19 pandamic her computer mic is not very good."
+    );
+}
+
+function anahita() {
+    window.alert(
+        "she joined at 5th grade she is good in hindi and math and science she is also a very good calculator (like arav) she even speaks properly."
+    );
+}
